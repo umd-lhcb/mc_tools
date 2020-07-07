c3c4d2ea41c00e835f54369ef8892970547a415f
Yipeng Sun <syp@umd.edu>
2020-07-07 19:56:00 +0800

    WIP #1: Tune the branch fraction to have 4 decimals.

--- bin/dec_print.py
+++ bin/dec_print.py
@@ -2,21 +2,51 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Jul 07, 2020 at 07:29 PM +0800
+# Last Change: Tue Jul 07, 2020 at 07:55 PM +0800
 
 import sys
+import operator
 
+from particle.converters import PDG2EvtGenNameMap
 from decaylanguage import DecFileParser
+from decaylanguage.dec.dec import get_final_state_particle_names, \
+    get_branching_fraction
 
 
 def list_complement(l_m, l_s):
     return [i for i in l_m if i not in l_s]
 
 
+class MyDecFileParser(DecFileParser):
+    def print_decay_modes(self, mother,
+                          pdg_name=False, print_model=True, ascending=False):
+        if pdg_name:
+            mother = PDG2EvtGenNameMap[mother]
+
+        dms = self._find_decay_modes(mother)
+
+        l = list()
+        if print_model:
+            for dm in dms:
+                dm_details = self._decay_mode_details(dm)
+                l.append((dm_details[0], '%-50s %15s %s' %
+                          ('  '.join(p for p in dm_details[1]), dm_details[2], dm_details[3])))
+        else:
+            for dm in dms:
+                fsp_names = get_final_state_particle_names(dm)
+                l.append((get_branching_fraction(dm),
+                         '%-50s' % ('  '.join(p for p in fsp_names))))
+
+        l.sort(key=operator.itemgetter(0), reverse=(not ascending))
+
+        for bf, info in l:
+            print('  %.4f : %s' % (bf, info))  # Manuel wants the BF to have 4 decimals.
+
+
 if __name__ == '__main__':
     dec_file = sys.argv[1]
 
-    parser = DecFileParser(dec_file)
+    parser = MyDecFileParser(dec_file)
     parser.parse()
 
     decays = list_complement(parser.list_decay_mother_names(),
