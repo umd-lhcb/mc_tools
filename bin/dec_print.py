1d9ea42e8dc7118b4d2f963e50a1c981cdd70a76
Yipeng Sun <syp@umd.edu>
2021-06-09 18:59:05 +0200

    Try to align everything!

--- bin/dec_print.py
+++ bin/dec_print.py
@@ -2,13 +2,14 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Tue Jun 08, 2021 at 08:51 PM +0200
+# Last Change: Wed Jun 09, 2021 at 06:58 PM +0200
 
 import sys
 import operator
 
 from particle.converters import PDG2EvtGenNameMap
 from decaylanguage import DecFileParser
+from itertools import zip_longest
 
 
 def list_complement(l_m, l_s):
@@ -17,33 +18,33 @@ def list_complement(l_m, l_s):
 
 class MyDecFileParser(DecFileParser):
     def print_decay_modes(self, mother,
-                          pdg_name=False, ascending=False):
+                          pdg_name=False, print_model=True, ascending=False,
+                          normalize=True):
         if pdg_name:
             mother = PDG2EvtGenNameMap[mother]
 
         dms = self._find_decay_modes(mother)
-        output_dms = list()
 
-        # Finding maximum length of decay string
-        maxlen = 0
+        ls_dict = dict()
         for dm in dms:
-            dm_details = self._decay_mode_details(dm)
-            decay = ('  '.join(p for p in dm_details[1]))
-            if(len(decay) > maxlen):
-                maxlen = len(decay)
+            bf, fsp_names, model, model_params = self._decay_mode_details(dm)
+            model_params = [str(i) for i in model_params]
+            ls_dict[bf] = (fsp_names, model, model_params)
 
-        width = str(maxlen + 5)  # Print the FF 5 spaces away from closest decay
-        format_string = '%-'+width+'s %s %s'
-        for dm in dms:
-            dm_details = self._decay_mode_details(dm)
-            output_dms.append((dm_details[0], format_string %
-                               ('  '.join(p for p in dm_details[1]), dm_details[2], " ".join(map(str, dm_details[3])))))
+        dec_details = list(ls_dict.values())
+        ls_attrs_aligned = list(zip_longest(
+            *[self._align_items(i) for i in zip(*dec_details)], fillvalue=''))
 
-        output_dms.sort(key=operator.itemgetter(0), reverse=(not ascending))
-        norm = sum([bf for bf, _ in output_dms])
+        ls = [(bf, ls_attrs_aligned[idx]) for idx, bf in enumerate(ls_dict)]
+        ls.sort(key=operator.itemgetter(0), reverse=(not ascending))
+        norm = sum([bf for bf, _ in ls]) if normalize else 1
 
-        for bf, info in output_dms:
-            print('  %.4f   %s;' % (bf/norm, info.rstrip()))  # Manuel wants the BF to be normalized and have 4 decimals.
+        for bf, info in ls:
+            if print_model:
+                line = '  %.4f   %s     %s  %s' % (bf/norm, *info)
+            else:
+                line = '  %.4f   %s' % (bf/norm, info[0])
+            print(line.rstrip()+';')
 
     def _decay_mode_details(self, decay_mode):
         bf, fsp_names, model, model_params = super()._decay_mode_details(
@@ -54,6 +55,37 @@ class MyDecFileParser(DecFileParser):
             model = 'PHOTOS ' + model
 
         return bf, fsp_names, model, model_params
+
+    @staticmethod
+    def _align_items(to_align, align_mode='left', sep=' '):
+        """
+        Left or right align all strings in a list to the same length.
+        By default string is space-broke into sub-strings and each set of sub-
+        string aligned individually.
+        """
+        if not isinstance(to_align[0], list):
+            max_len = max(len(s) for s in to_align)
+            if align_mode == 'left':
+                return [s.ljust(max_len) for s in to_align]
+            elif align_mode == 'right':
+                return [s.rjust(max_len) for s in to_align]
+            else:
+                raise ValueError('Unknown align mode: {}'.format(align_mode))
+
+        aligned = []
+        for cat in zip_longest(*to_align, fillvalue=''):
+            max_len = max(len(s) for s in cat)
+
+            if align_mode == 'left':
+                row = [s.ljust(max_len) for s in cat]
+            elif align_mode == 'right':
+                row = [s.ljust(max_len) for s in cat]
+            else:
+                raise ValueError('Unknown align mode: {}'.format(align_mode))
+
+            aligned.append(row)
+
+        return [sep.join(row) for row in zip(*aligned)]
 
 
 if __name__ == '__main__':
