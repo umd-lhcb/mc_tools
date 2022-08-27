ffff46d24d3cf189f1aab42fb322e7c416910a50
Yipeng Sun <syp@umd.edu>
2022-08-26 20:58:16 -0400

    Default to print BFs normalized.

--- bin/dec_print.py
+++ bin/dec_print.py
@@ -2,7 +2,7 @@
 #
 # Author: Yipeng Sun
 # License: BSD 2-clause
-# Last Change: Fri Jun 11, 2021 at 04:44 PM +0200
+# Last Change: Fri Aug 26, 2022 at 08:55 PM -0400
 
 from argparse import ArgumentParser
 from decaylanguage import DecFileParser
@@ -17,6 +17,8 @@ def parse_input():
                         help="don't print PHOTOS.")
     parser.add_argument('-m', '--no-model', action='store_false',
                         help="don't print decay model.")
+    parser.add_argument('-N', '--no-normalize', action='store_false',
+                        help="don't normalize branch fractions.")
 
     return parser.parse_args()
 
@@ -37,4 +39,6 @@ if __name__ == '__main__':
     for d in decays:
         print('Decay {}'.format(d))
         parser.print_decay_modes(
-            d, display_photos_keyword=args.no_photos, print_model=args.no_model)
+            d, display_photos_keyword=args.no_photos, print_model=args.no_model,
+            normalize=args.no_normalize
+        )
